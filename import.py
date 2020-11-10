cf454ba0bca11cb4253f595e0b30ca6573eb1b69
Antonio <Tenundor@gmail.com>
2020-11-10 18:25:17 +0300

    Step 12/17 from lesson 1 completed https://dvmn.org/modules/website-layout-for-pydev/lesson/wine-shop/#12

--- import.py
+++
@@ -1,7 +0,0 @@
-import pandas
-from pprint import pprint
-
-
-excel_data = pandas.read_excel('wine2.xlsx', sheet_name='Лист1')
-
-pprint(excel_data.to_dict(orient='records'))
--- main.py
+++ main.py
@@ -27,6 +27,14 @@ def plural_years_rus(year):
 
 excel_data_wine = pandas.read_excel('wine.xlsx', sheet_name='Лист1').to_dict(orient='records')
 
+excel_wine2_df = pandas.read_excel('wine2.xlsx', sheet_name='Лист1')
+wine2_category = excel_wine2_df['Категория'].unique().tolist()
+wine2_dict = {}
+for category in wine2_category:
+    wine2_dict.update({category: excel_wine2_df[excel_wine2_df.Категория == category]
+                      [['Картинка', 'Название', 'Сорт', 'Цена']].to_dict(orient='records')})
+pprint(wine2_dict)
+
 
 rendered_page = template.render(wines=excel_data_wine)
 
