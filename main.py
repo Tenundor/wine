18c0f35a2cb4b00c54b482090f7d9f7949d78f3d
Antonio <Tenundor@gmail.com>
2020-11-12 19:40:41 +0300

    Completed 14, 15 https://dvmn.org/modules/website-layout-for-pydev/lesson/wine-shop/#15

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 import datetime
 import pandas
 from pprint import pprint
+from collections import OrderedDict
 
 
 env = Environment(
@@ -28,17 +29,17 @@ def plural_years_rus(year):
 
 excel_data_wine = pandas.read_excel('wine.xlsx', sheet_name='Лист1').to_dict(orient='records')
 
-excel_wine2_df = pandas.read_excel('wine2.xlsx', sheet_name='Лист1')
-print(excel_wine2_df)
+excel_wine2_df = pandas.read_excel('wine2.xlsx', sheet_name='Лист1').fillna('blank')
 wine2_category = excel_wine2_df['Категория'].unique().tolist()
 wine2_dict = {}
 for category in wine2_category:
     wine2_dict.update({category: excel_wine2_df[excel_wine2_df.Категория == category]
                       [['Картинка', 'Название', 'Сорт', 'Цена']].to_dict(orient='records')})
+wine2_ordered_dict = OrderedDict(sorted(wine2_dict.items()))
 pprint(wine2_dict)
 
 
-rendered_page = template.render(wines=excel_data_wine)
+rendered_page = template.render(wines=excel_data_wine, wine_categories=wine2_ordered_dict)
 
 with open('index.html', 'w', encoding="utf8") as file:
     file.write(rendered_page)
