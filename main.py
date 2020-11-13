742bd2d1ca520444cddf9b78c7a9204e5c193568
tenundor@gmail.com <He5LA90m7a9T>
2020-11-13 13:31:18 +0300

    Completed tasks 16, 17 fixed bug with company age

--- main.py
+++ main.py
@@ -17,8 +17,8 @@ foundation_year = 1920
 company_age = datetime.datetime.today().year - foundation_year
 
 
-def plural_years_rus(year):
-    """returns the word "год" in the required case"""
+def change_year_word_endings_rus(year):
+    """Returns the russian word "Год" with an ending that matches the specified number "year" """
     if str(year)[-1] in ['2', '3', '4']:
         return 'года'
     if str(year)[-1] == '1':
@@ -27,19 +27,20 @@ def plural_years_rus(year):
         return 'лет'
 
 
-excel_data_wine = pandas.read_excel('wine.xlsx', sheet_name='Лист1').to_dict(orient='records')
-
-excel_wine2_df = pandas.read_excel('wine2.xlsx', sheet_name='Лист1').fillna('blank')
-wine2_category = excel_wine2_df['Категория'].unique().tolist()
-wine2_dict = {}
-for category in wine2_category:
-    wine2_dict.update({category: excel_wine2_df[excel_wine2_df.Категория == category]
-                      [['Картинка', 'Название', 'Сорт', 'Цена']].to_dict(orient='records')})
-wine2_ordered_dict = OrderedDict(sorted(wine2_dict.items()))
-pprint(wine2_dict)
+excel_wine_df = pandas.read_excel('wine3.xlsx', sheet_name='Лист1').fillna('blank')
+wine_category = excel_wine_df['Категория'].unique().tolist()
+wine_dict = {}
+for category in wine_category:
+    wine_dict.update({category: excel_wine_df[excel_wine_df.Категория == category]
+                     .drop(['Категория'], axis=1).head().to_dict(orient='records')})
+wine_ordered_dict = OrderedDict(sorted(wine_dict.items()))
+pprint(wine_dict)
 
 
-rendered_page = template.render(wines=excel_data_wine, wine_categories=wine2_ordered_dict)
+rendered_page = template.render(wine_categories=wine_ordered_dict,
+                                company_age="Уже {} {} с вами".format(
+                                    company_age, change_year_word_endings_rus(company_age)),
+                                )
 
 with open('index.html', 'w', encoding="utf8") as file:
     file.write(rendered_page)
