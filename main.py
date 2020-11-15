f975be3bfa7c35094f2026cdc0d6397635e13796
tenundor@gmail.com <He5LA90m7a9T>
2020-11-15 22:20:45 +0300

    Added the ability to specify the path to a table with a list of wines as an argument `--wine_path` to main.py

--- main.py
+++ main.py
@@ -7,11 +7,23 @@ from collections import OrderedDict
 import argparse
 
 
+def change_year_word_endings_rus(year):
+    """Returns the russian word "Год" with an ending that matches the specified number "year" """
+    if str(year)[-1] in ['2', '3', '4']:
+        return 'года'
+    if str(year)[-1] == '1':
+        return 'год'
+    else:
+        return 'лет'
+
+
 parser = argparse.ArgumentParser(
     description='''Программа формирует страницу сайта-магазина по продаже крымских вин
-    на основе шаблона template.html и таблицы со списком вин в формате .xlsx''')
+    на основе шаблона template.html и таблицы со списком вин в формате .xlsx'''
+)
 parser.add_argument(
-    '--wine_path', help='''Путь к файлу cо списком продукции''', default='wine.xlsx')
+    '--wine_path', help='''Путь к файлу cо списком продукции''', default='wine.xlsx'
+)
 wine_path = parser.parse_args().wine_path
 
 env = Environment(
@@ -24,17 +36,6 @@ template = env.get_template('template.html')
 foundation_year = 1920
 company_age = datetime.datetime.today().year - foundation_year
 
-
-def change_year_word_endings_rus(year):
-    """Returns the russian word "Год" with an ending that matches the specified number "year" """
-    if str(year)[-1] in ['2', '3', '4']:
-        return 'года'
-    if str(year)[-1] == '1':
-        return 'год'
-    else:
-        return 'лет'
-
-
 excel_wine_df = pandas.read_excel(wine_path, sheet_name='Лист1').fillna('blank')
 wine_category = excel_wine_df['Категория'].unique().tolist()
 wine_dict = {}
@@ -43,7 +44,6 @@ for category in wine_category:
                      .drop(['Категория'], axis=1).head().to_dict(orient='records')})
 wine_ordered_dict = OrderedDict(sorted(wine_dict.items()))
 pprint(wine_dict)
-
 
 rendered_page = template.render(wine_categories=wine_ordered_dict,
                                 company_age="Уже {} {} с вами".format(
