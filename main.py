e2ea8e272815c6fddbbc0f0cee846feca28bd2f1
tenundor@gmail.com <He5LA90m7a9T>
2020-11-15 23:11:29 +0300

    unpack {% for wine in wine_categories[wine_category] %} in template.html:121

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
+import argparse
+from collections import OrderedDict
+import datetime
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
-import datetime
 import pandas
-from pprint import pprint
-from collections import OrderedDict
-import argparse
 
 
 def change_year_word_endings_rus(year):
@@ -36,15 +35,22 @@ template = env.get_template('template.html')
 foundation_year = 1920
 company_age = datetime.datetime.today().year - foundation_year
 
-wines_description_df = pandas.read_excel(wine_path, sheet_name='Лист1').fillna('blank')
+wines_description_df = pandas.read_excel(
+    wine_path, sheet_name='Лист1').fillna('blank')
 wine_categories = wines_description_df['Категория'].unique().tolist()
 wines_description_by_categories = {}
 for category in wine_categories:
-    wines_description_by_categories.update({category: wines_description_df[wines_description_df.Категория == category]
-                                           .drop(['Категория'], axis=1).head().to_dict(orient='records')})
-wines_description_by_categories_sorted = OrderedDict(sorted(wines_description_by_categories.items()))
+    wines_in_category = wines_description_df[wines_description_df.Категория == category].drop(
+        ['Категория'], axis=1
+    )
+    wines_description_by_categories.update(
+        {category: wines_in_category.to_dict(orient='records')}
+    )
+wines_description_by_categories_sorted = OrderedDict(
+    sorted(wines_description_by_categories.items())
+)
 
-rendered_page = template.render(wine_categories=wines_description_by_categories_sorted,
+rendered_page = template.render(wines_by_categories=wines_description_by_categories_sorted,
                                 company_age="Уже {} {} с вами".format(
                                     company_age, change_year_word_endings_rus(company_age)),
                                 )
